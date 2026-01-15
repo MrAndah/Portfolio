17c54c142cd99cd121f2b010c3542091296448b3
Godfrey Andah <andahgodfrey4@gmail.com>
2026-01-15 21:18:23 +0000

    completed the skills schema

--- src/about.jsx
+++ src/about.jsx
@@ -6,9 +6,29 @@ function Skills() {
             <div className="skills-container">
                 <div className="skills-heading"> Skills & Expertise </div>
                 <div className="skills-content">
-                    <div className={"skills-div"}></div>
-                    <div className={"skills-div"}></div>
-                    <div className={"skills-div"}></div>
+                    <div className={"skills-div"}>
+                        <div className={'title'}> Frontend Development</div>
+                        <ul className="skills-list">
+                            <li> HTML</li>
+                            <li> CSS</li>
+                            <li> Javascript</li>
+                            <li> REACT</li>
+                        </ul>
+                    </div>
+                    <div className={"skills-div"}>
+                        <div className={'title'}> Backend Development</div>
+                        <ul className="skills-list">
+                            <li> Sql</li>
+                            <li> Java (SpringBoot)</li>
+                        </ul>
+                    </div>
+                    <div className={"skills-div"}>
+                        <div className={'title'}> Tools</div>
+                        <ul className="skills-list">
+                            <li> Git & Github</li>
+                            <li> Docker</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </section>
