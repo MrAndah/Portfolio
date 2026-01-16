d5f1f169c4a34bd23ec8d3d2a2c6290bbb052b4a
Godfrey Andah <andahgodfrey4@gmail.com>
2026-01-16 17:31:06 +0000

    Finished contact section

--- src/contact.jsx
+++ src/contact.jsx
@@ -5,31 +5,25 @@ function Contact () {
     return(
         <section id="contact">
             <p className="section__text__p1">Get in Touch</p>
-            <h1 className="title">Contact Me</h1>
-            <div className="contact-info-upper-container">
-                <div className="contact-info-container">
-                    <img
-                        src="./src/assets/email.png"
-                        alt="Email icon"
-                        className="icon contact-icon email-icon"
-                    />
-                    <p><a href="mailto:andahgodfrey4@gmail.com">Email Me</a></p>
-                </div>
-                <div className="contact-info-container">
-                    <img
-                        src="./src/assets/linkedin.png"
-                        alt="LinkedIn icon"
-                        className="contact-icon"
-                    />
-                    <p><a
-                        href="https://www.linkedin.com/in/godfrey-andah?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
-                        target='_blank'>LinkedIn</a></p>
-                </div>
+            <h1 className="contact-title">Contact Me</h1>
+            <div className="contact-icon-container">
+                <a href="mailto:andahdgodfrey4@gmail.com">
+                    <img className="contact-icon" src="./src/assets/email.svg" alt="email" />
+                </a>
+                <a href="https://github.com/MrAndah" target="_blank" >
+                    <img className="contact-icon" src="./src/assets/github.png" alt="github icon" />
+                </a>
+                <a href="https://www.linkedin.com/in/godfrey-andah/">
+                    <img className="contact-icon" src="./src/assets/linkedin.png" alt="linkedin icon" />
+                </a>
+                <a href="https://www.youtube.com/@GodfreyAndah" target="_blank">
+                    <img className="contact-icon" src="./src/assets/youtube.svg" alt="youtube icon" />
+                </a>
             </div>
             <footer>
                 <div>
                     <div className="nav-links-container">
-                        <ul className="navlinks">
+                        <ul className="contact-navlinks">
                             <li><a href="#home">About</a></li>
                             <li><a href="#skills">Experience</a></li>
                             <li><a href="#projects">Projects</a></li>
@@ -37,7 +31,7 @@ function Contact () {
                         </ul>
                     </div>
                 </div>
-                <p>Copyright &#169;  Godfrey Andah. All Rights Reserved.</p>
+                <p> &#169; 2026 Godfrey Kobina Andah. All Rights Reserved.</p>
             </footer>
         </section>
 
