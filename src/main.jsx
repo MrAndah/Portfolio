84810a5f8153bd80b6208b736d581cd373459b2a
Godfrey Andah <andahgodfrey4@gmail.com>
2026-01-16 18:50:44 +0000

    Allow code to be visible

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import Skills from './about.jsx'
 import Contact from './contact.jsx'
 import ReactDOM from 'react-dom/client';
 import FadeIn from "./FadeIn.jsx";
+import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <FadeIn duration={250}>
-            <Home/>
-      </FadeIn>
-            <Skills/>
-      <FadeIn duration={250}>
-            <Contact/>
-      </FadeIn>
+      <HashRouter>
+            <FadeIn duration={250}>
+                  <Home/>
+            </FadeIn>
+                  <Skills/>
+            <FadeIn duration={250}>
+                  <Contact/>
+            </FadeIn>
+      </HashRouter>
   </StrictMode>,
 )
